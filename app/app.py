8759714d16a4b2883360a5ae8137edc9102a9aab
HU-A-U <hu_a_u@163.com>
2019-06-12 16:20:55 +0800

    提交

---
+++ app/app.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+from flask import Flask
+from flask import request,jsonify
+app = Flask(__name__)
+
+#建立redis连接池
+import redis
+redis_pool = redis.ConnectionPool(host='127.0.0.1',port=6379,decode_responses=True)
+r = redis.Redis(connection_pool=redis_pool)
+
+@app.route('/', methods=['get'])
+def save_ip():
+    ip_addr = request.access_route[0]
+    msg = {}
+    if r.set(ip_addr,'pa'):
+        msg = {'msg':'ok'}
+
+    return jsonify(**msg)
+
+if __name__ == '__main__':
+    app.run()
